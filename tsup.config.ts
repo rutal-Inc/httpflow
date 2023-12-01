ff4ca21fbf3edf21fb4b3a6f6381eb2903fb2966
Talha Meer <talha.m@techacos.com>
2023-12-01 20:43:50 +0500

    add few more flags in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,11 @@ import { Options, defineConfig } from "tsup";
 
 export default defineConfig((options): Options => {
   return {
-    // minify: true,
+    minify: true,
     entry: ["src/index.ts"],
     format: ["cjs", "esm"],
+    dts: true,
+    sourcemap: true,
+    clean: true,
   };
 });
